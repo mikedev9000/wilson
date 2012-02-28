260ec02b5227218f15e99b2eb97d8e9e7ce2112d
mikedev9000 <michael1.cis@gmail.com>
2012-02-27 21:11:03 -0500

    Adding wilson\data\Model

---
+++ data/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace wilson\data;
+
+/**
+ * Model to be used within applications. Separated from lithium so that 
+ * it can be customized
+*/
+class Model extends \lithium\data\Model {
+	
+}
