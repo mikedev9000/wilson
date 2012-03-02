cbe758813e12cb7a5263b1ffd856927cb2940526
mikedev9000 <michael1.cis@gmail.com>
2012-03-01 21:11:24 -0500

    Adding the custom data\Model class to extend lithium\data\Model

--- data/Model.php
+++ data/Model.php
@@ -3,9 +3,13 @@
 namespace wilson\data;
 
 /**
- * Model to be used within applications. Separated from lithium so that 
+ * Model to be used within applications. Separated from lithium so that
  * it can be customized
 */
 class Model extends \lithium\data\Model {
-	
+
+    public function getObjectType( $entity ){
+        return get_called_class();
+    }
+
 }
